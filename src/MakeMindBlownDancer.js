276a96b5a8ca0d2198dd9a2e23c5bbe03c5d2cd9
ANAIT <Anait@ANAITs-MacBook-Pro.local>
2018-12-13 17:33:50 -0800

    Complete background image

--- src/MakeMindBlownDancer.js
+++ src/MakeMindBlownDancer.js
@@ -1,5 +1,7 @@
 var MakeMindBlownDancer = function(top, left, timeBetweenSteps) {
   MakeDancer.call(this, top, left, timeBetweenSteps);
+  this.$node = $('<span class="mindBlownDancer"></span>');
+  this.setPosition(top, left);
 };
 
 MakeMindBlownDancer.prototype = Object.create(MakeDancer.prototype); 
